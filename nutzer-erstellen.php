099545af74c903283725d6df21a3c48e3efef24e
Noa <nb080@hdm-stuttgart.de>
2017-07-07 20:22:57 +0200

    nutzer-erstellen Test

--- nutzer-erstellen.php
+++ nutzer-erstellen.php
@@ -1,5 +1,5 @@
 <?php
-    $pdo = new PDO('mysql:host=mars.iuk.hdm-stuttgart.de;dbname=u-ch126;charset=utf8', 'ch126', 'OhK5aepair');
+    $pdo = new PDO('ch126', 'mysql:host=mars.iuk.hdm-stuttgart.de;dbname=u-ch126;charset=utf8', 'OhK5aepair');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
 /**
