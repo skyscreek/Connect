5d56cd01460e2a4ddee07704ce9914693434a446
Chris <ch126@hdm-stuttgart.de>
2017-07-08 13:45:34 +0200

    Anpassung Registrierung & funktionierende Anmeldung (ohne Coockies)

--- anmeldung.php
+++ anmeldung.php
@@ -1,28 +1,31 @@
 <?php
-include('Klassen/DB.php');
-if (isset($_POST['anmeldung'])) {
+include('klassen/DB.php');
+
+if (isset($_POST['login'])) {
+
     $username = $_POST['username'];
     $password = $_POST['password'];
+
     if (DB::query('SELECT username FROM nutzer WHERE username=:username', array(':username'=>$username))) {
+
         if (password_verify($password, DB::query('SELECT password FROM nutzer WHERE username=:username', array(':username'=>$username))[0]['password'])) {
-            echo 'Willkommen!';
+            echo 'Willkommen bei connect!';
+
         } else {
-            echo 'Falsches Passwort!';
+
+            echo 'Dein Passwort ist falsch!';
         }
+
     } else {
-        echo 'Nutzer existiert nicht!';
+        echo 'Dieser Benutzer existiert nicht!';
     }
 }
 ?>
-<h1>Login to your account</h1>
+<h1>Bei Connect anmelden</h1>
 <form action="anmeldung.php" method="post">
-    <input type="text" name="username" value="" placeholder="Nutzername ..."><p />
+    <input type="text" name="username" value="" placeholder="Benutzername ..."><p />
     <input type="password" name="password" value="" placeholder="Passwort ..."><p />
     <input type="submit" name="login" value="Anmelden">
 </form>
-/**
- * Created by PhpStorm.
- * User: Noa
- * Date: 07.07.17
- * Time: 18:54
- */
+
+
--- nutzer-erstellen.php
+++ nutzer-erstellen.php
@@ -36,7 +36,7 @@ if (isset($_POST['nutzer-erstellen'])) {
 
         }
     } else {
-        echo 'Dieser Benutzer existiert bereits!';
+        echo 'Dieser Benutzer existiert schon!';
     }
 }
 ?>
