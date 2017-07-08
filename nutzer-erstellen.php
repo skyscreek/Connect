a84ce7700d905d9efdb7110aa86f7c9c137e1cc3
Chris <ch126@hdm-stuttgart.de>
2017-07-08 12:54:13 +0200

    Registrierung aktualisiert, mit Passwort, Email, Benutzername und existierender Benutzer check

--- nutzer-erstellen.php
+++ nutzer-erstellen.php
@@ -4,15 +4,36 @@ if (isset($_POST['nutzer-erstellen'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
     $email = $_POST['email'];
-    DB::query('INSERT INTO nutzer VALUES (\'\', :username, :password, :email)', array(':username'=>$username, ':password'=>$password, ':email'=>$email));
-    echo "Hasts geschafft!";
+    if (!DB::query('SELECT username FROM users WHERE username=:username', array(':username'=>$username))) {
+        if (strlen($username) >= 3 && strlen($username) <= 32) {
+            if (preg_match('/[a-zA-Z0-9_]+/', $username)) {
+                if (strlen($password) >= 6 && strlen($password) <= 60) {
+                    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                        DB::query('INSERT INTO users VALUES (\'\', :username, :password, :email)', array(':username'=>$username, ':password'=>password_hash($password, PASSWORD_BCRYPT), ':email'=>$email));
+                        echo "Deine Registrierung war erfolgreich!";
+                    } else {
+                        echo 'Deine Email-Adresse ist falsch!';
+                    }
+                } else {
+                    echo 'Dein Passwort ist falsch!';
+                }
+            } else {
+                echo 'Dein Benutzername ist falsch';
+            }
+        } else {
+            echo 'Dein Benutzername ist falsch';
+        }
+    } else {
+        echo 'Dieser Benutzer existiert bereits!';
+    }
 }
 ?>
 
-<h1>Register</h1>
+
+<h1>Jetzt registrieren!</h1>
 <form action="nutzer-erstellen.php" method="post">
-    <input type="text" name="username" value="" placeholder="Username ..."><p />
-    <input type="password" name="password" value="" placeholder="Password ..."><p />
-    <input type="email" name="email" value="" placeholder="someone@somesite.com"><p />
+    <input type="text" name="username" value="" placeholder="Benutzername ..."><p />
+    <input type="password" name="password" value="" placeholder="Passwort ..."><p />
+    <input type="email" name="email" value="" placeholder="email@mailanbieter.de"><p />
     <input type="submit" name="nutzer-erstellen" value="Nutzer Erstellen">
 </form>
