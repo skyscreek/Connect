fed5138cf428fea3d943902383b7cb5221663d0a
Chris <ch126@hdm-stuttgart.de>
2017-07-10 21:12:19 +0200

    Folgen und entfolgen geht jetzt, zwar etwas komisch, kann aber sein dass das später besser geht

--- profil.php
+++ profil.php
@@ -2,7 +2,7 @@
 include('./klassen/DB.php');
 include('./klassen/anmeldung_klasse.php');
 $nutzername = "";
-$folgt = False;
+$folgtgerade = False;
 if (isset($_GET['nutzername'])) {
     if (DB::query('SELECT nutzername FROM nutzer WHERE nutzername=:nutzername', array(':nutzername'=>$_GET['nutzername']))) {
         $nutzername = DB::query('SELECT nutzername FROM nutzer WHERE nutzername=:nutzername', array(':nutzername'=>$_GET['nutzername']))[0]['nutzername'];
@@ -15,7 +15,7 @@ if (isset($_GET['nutzername'])) {
                 } else {
                     echo 'Du folgst diesem Nutzer bereits!';
                 }
-                $folgt = True;
+                $folgtgerade = True;
             }
         }
         if (isset($_POST['entfolgen'])) {
@@ -23,12 +23,12 @@ if (isset($_GET['nutzername'])) {
                 if (DB::query('SELECT folger_id FROM folger WHERE nutzer_id=:nutzerid', array(':nutzerid'=>$nutzerid))) {
                     DB::query('DELETE FROM folger WHERE nutzer_id=:nutzerid AND folger_id=:folgerid', array(':nutzerid'=>$nutzerid, ':folgerid'=>$folgerid));
                 }
-                $folgt = False;
+                $folgtgerade = False;
             }
         }
         if (DB::query('SELECT folger_id FROM folger WHERE nutzer_id=:nutzerid', array(':nutzerid'=>$nutzerid))) {
             //echo 'Already following!';
-            $folgt = True;
+            $folgtgerade = True;
         }
     } else {
         die('Nutzer wurde nicht gefunden!');
@@ -39,10 +39,10 @@ if (isset($_GET['nutzername'])) {
 <form action="profil.php?nutzername=<?php echo $nutzername; ?>" method="post">
     <?php
     if ($nutzerid != $folgerid) {
-        if ($folgt) {
-            echo '<input type="submit" name="entfolgen" value="Entfolgen">';
+        if ($folgtgerade) {
+            echo '<input type="submit" name="entfolgen" value="entfolgen">';
         } else {
-            echo '<input type="submit" name="folgen" value="Folgen">';
+            echo '<input type="submit" name="folgt" value="folgen">';
         }
     }
     ?>
